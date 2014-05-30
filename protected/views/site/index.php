afbe66357a5f43d14a2940590116a964face0d63
wauout <neo16@sapo.pt>
2014-05-31 00:32:14 +0100

    changed hint text on the login form

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -19,4 +19,4 @@ the <a href="http://www.yiiframework.com/doc/">documentation</a>.
 Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
 should you have any questions.</p> -->
 
-This is the indes of our WebApp! Enjoy it!
+This is the index of our WebApp! Enjoy it!
